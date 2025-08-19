70f38cb597260128c61a40f79acd3f45ff21a7e5
hdavid1206 <palenciacastroharolddavid@gmail.com>
2025-08-19 11:35:54 -0500

    realize el boton de apoyos en el inicio.html

--- sena_app_project/apoyos/urls.py
+++ sena_app_project/apoyos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'apoyos'
+
 urlpatterns = [
-    path('apoyos/', views.apoyos, name='apoyos'),
+    path('apoyos/', views.apoyos, name='lista_apoyos'),
 ]
